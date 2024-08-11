1ce2c9b7078d4abd3682ca784cc8494f29c6b0c3
bluejazx <josh.jay.724@gmail.com>
2024-08-11 14:52:15 -0400

    project finished

--- functions.cpp
+++ functions.cpp
@@ -34,7 +34,7 @@ void GameRender(string playerName)
     //Creates walls, snakehead, and food
     for(int i = 0; i < Global::height; i++)
     {
-        for(int j = 0; j<= Global::width; j++)
+        for(int j = 0; j <= Global::width; j++)
         {
             //Creates the side walls 
             if(j == 0 || j == Global::width)
@@ -83,4 +83,143 @@ void GameRender(string playerName)
     //Displays players score 
 
     cout << playerName << "'s Score: " << Global::playerScore << endl;
+}
+
+void UpdateGame()
+{
+    //Movess the snakes body guided the head along x,y axis
+    int prevX = Global::snakeTailX[0];
+    int prevY = Global::snakeTailY[0];
+    int prev2X, prev2Y;
+    Global::snakeTailX[0] = Global::snakeCordX;
+    Global::snakeTailY[0] = Global::snakeCordY;
+
+    for (int i = 1; i < Global::snakeTailLen; i++)
+    {
+        prev2X = Global::snakeTailX[i];
+        prev2Y = Global::snakeTailY[i];
+        Global::snakeTailX[i] = prevX;
+        Global::snakeTailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
+    //Changes player direction based on inputs
+    switch(Global::sDir)
+    {
+        case Global::LEFT:
+        {
+            Global::snakeCordX--;
+            break;
+        }
+        case Global::RIGHT:
+        {
+            Global::snakeCordX++;
+            break;
+        }
+        case Global::UP:
+        {
+            Global::snakeCordY--;
+            break;
+        }
+        case Global::DOWN:
+        {
+            Global::snakeCordY++;
+            break;
+        }
+    }
+
+    //Checks for snake collision with the wall (|)
+    if (Global::snakeCordX >= Global::width || Global::snakeCordX < 0 || Global::snakeCordY > Global::height || Global::snakeCordY < 0)
+    {
+        Global::isGameOver = true;
+    }
+
+    //Checks for collision with the tail (o)
+    for (int i = 0; i < Global::snakeTailLen; i++)
+    {
+        if (Global::snakeTailX[i] == Global::snakeCordX && Global::snakeTailY[i] == Global::snakeCordY)
+        {
+            Global::isGameOver = true;
+        }
+    }
+
+    //Checks for snake's collision with the food (#)
+    if (Global::snakeCordX == Global::fruitCordX && Global::snakeCordY == Global::fruitCordY)
+    {
+        Global::playerScore += 10;
+        Global::fruitCordX = rand() % Global::width;
+        Global::fruitCordY = rand() % Global::height;
+        Global::snakeTailLen++;
+    }
+}
+
+int SetDifficulty()
+{
+    int dfc, choice; 
+    cout << "\nSET DIFFICULTY\n1: Easy\n2: Medium\n3: hard "
+            "\nNOTE: if not chosen or pressed any other "
+            "key, the difficulty will be automatically set "
+            "to medium\nChoose difficulty level: "; 
+    cin >> choice;
+    switch(choice)
+    {
+        case 1:
+        {    
+            dfc = 50;
+            break;
+        }
+        case 2:
+        {    
+            dfc = 100;
+            break;
+        }
+        case 3:
+        {    
+            dfc = 100;
+            break;
+        }
+        default:
+        {
+            dfc = 100;
+        }
+    }
+    return dfc;
+}
+
+void UserInput()
+{
+    //Checks if key has been pressed or not
+    if(_kbhit())
+    {
+        //Gets the key that has been press
+        switch(_getch())
+        {
+            case 'a':
+            {
+                Global::sDir = Global::LEFT;
+                break;
+            }
+            case 'd':
+            {
+                Global::sDir = Global::RIGHT;
+                break;
+            }
+             case 'w':
+            {
+                Global::sDir = Global::UP;
+                break;
+            }
+             case 's':
+            {
+                Global::sDir = Global::DOWN;
+                break;
+            }
+             case 'x':
+            {
+                Global::isGameOver = true;
+                break;
+            }
+        }
+    }
 }
--- functions.hpp
+++ functions.hpp
@@ -13,3 +13,12 @@ void GameInit();
 
 //Function for creating the game board & rendering
 void GameRender(string playerName);
+
+//Function for updating the game state
+void UpdateGame();
+
+//Function for setting the gmaes difficulty
+int SetDifficulty();
+
+//Function for taking user input, and input handiling
+void UserInput();
